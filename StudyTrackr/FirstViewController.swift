d7190b783fad852ecdf3c945b56a92731a1a1553
jdslomka <jdslomka@zpr-l0007072.local>
2017-04-27 10:27:41 -0400

    Initial Commit

---
+++ StudyTrackr/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  StudyTrackr
+//
+//  Created by John Slomka on 2017-04-27.
+//  Copyright © 2017 John Slomka. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
