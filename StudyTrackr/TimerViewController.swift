fd038ab3f0816b905290a1ebd76a4108513973d7
Nadia Lunz <nadialunz@Nadias-MacBook-Air.local>
2017-05-03 10:15:22 -0400

    Created almost working timer

--- StudyTrackr/TimerViewController.swift
+++ StudyTrackr/TimerViewController.swift
@@ -7,8 +7,53 @@
 //
 
 import UIKit
-
-class TimerViewController: UIViewController {
+    
+    class TimerViewController: UIViewController {
+        
+        @IBOutlet weak var timerLabel: UILabel!
+        
+        var seconds = 70
+        var timer = Timer()
+        var isTimerRunning = false
+        var resumeTapped = false
+        
+        @IBAction func startButtonTapped(_ sender: UIButton) {
+            if isTimerRunning == false{
+                runTimer()
+            }
+        }
+        
+        @IBAction func pauseButtonTapped(_ sender: UIButton) {
+            if self.resumeTapped == false{
+                timer.invalidate()
+                self.resumeTapped = true
+            }else{
+                self.resumeTapped = false
+            }
+        }
+        
+        @IBAction func doneButtonTapped(_ sender: UIButton) {
+            timer.invalidate()
+            seconds = 60
+            timerLabel.text = String(seconds)
+        }
+        
+        func runTimer(){
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(TimerViewController.updateTimer)), userInfo: nil, repeats: true)
+        }
+        
+        func updateTimer() {
+            seconds -= 1
+            timerLabel.text = timeString(time: TimeInterval(seconds))
+        }
+        
+        func timeString(time:TimeInterval) -> String {
+            let hours = Int(time) / 3600
+            let minutes = Int(time) / 60 % 60
+            let seconds = Int(time) % 60
+            return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
+        }
+        
 
     override func viewDidLoad() {
         super.viewDidLoad()
