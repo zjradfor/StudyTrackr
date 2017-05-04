d273a65b24ce3b210c7a4a763038be87cb836ea5
reado1413 <reado1413@googleapps.wrdsb.ca>
2017-05-04 10:22:47 -0400

    Update

--- StudyTrackr/CalendarCell.swift
+++ StudyTrackr/CalendarCell.swift
@@ -10,21 +10,26 @@ import UIKit
 
 class CalendarCell: UICollectionViewCell {
     var textLabel: UILabel
-    
+    var button: UIButton
     
     override init(frame: CGRect){
-        textLabel = UILabel(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
-        
+        textLabel = UILabel(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height/2))
+        button = UIButton(frame: CGRect(x: frame.size.width / 2, y: frame.size.height / 2, width: frame.size.width, height: frame.size.height)
         super.init(frame: frame)
         //x: 0, y: 20
-        
-        textLabel = UILabel(frame: CGRect(x: 0, y: -20, width: frame.size.width, height: frame.size.height))
+        textLabel = UILabel(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height/2))
         textLabel.font = UIFont.systemFont(ofSize: UIFont.systemFontSize)
         textLabel.textAlignment = .natural
         textLabel.textColor = UIColor.darkText
         contentView.addSubview(textLabel)
+        button = UIButton(frame: CGRect(x: frame.size.width / 2, y: frame.size.height / 2, width: frame.size.width, height: frame.size.height)
+        button.addTarget(self, action: #selector(ratingButtonTapped), for: .touchUpInside)
     }
     
+    func ratingButtonTapped() {
+         print("Button Pressed!")
+    }
+                          
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
