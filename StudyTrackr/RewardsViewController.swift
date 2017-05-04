5aeefb30a9ed28ce809079c25477c865139ad158
Dylan Smith <Dylan0327@zpr-l0007097.local>
2017-05-04 10:48:29 -0400

    added extra branches to our main tab,
    - tabbar disappears when going back to main rewards branch however

---
+++ StudyTrackr/RewardsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RewardsViewController.swift
+//  StudyTrackr
+//
+//  Created by Dylan Smith on 2017-05-04.
+//  Copyright © 2017 John Slomka. All rights reserved.
+//
+
+import UIKit
+
+class RewardsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+self.tabBarController?.tabBar.isHidden = false
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
