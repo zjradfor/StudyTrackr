37651a9813d3971e0468848f4118b7e09de56423
Dylan Smith <Dylan0327@zpr-l0007097.local>
2017-05-08 10:22:32 -0400

    hiding tab bar and creating a back button to send us to our main screen once it exists.

--- StudyTrackr/RewardsViewController.swift
+++ StudyTrackr/RewardsViewController.swift
@@ -12,7 +12,8 @@ class RewardsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-self.tabBarController?.tabBar.isHidden = false
+self.tabBarController?.tabBar.isHidden = true // cannot make tabbar stay, so we will force it to be hidden and then create a button used in order to go back to our main menu screen once it it exists
+        
         // Do any additional setup after loading the view.
     }
 
