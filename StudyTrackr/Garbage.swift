8121cdf9e6955d106a4863fd1dd2e8b205f166a4
jdslomka <jdslomka@zpr-l0007072.local>
2017-05-12 10:44:44 -0400

    Garbage

---
+++ StudyTrackr/Garbage.swift
@@ -0,0 +1,35 @@
+//
+//  Garbage.swift
+//  StudyTrackr
+//
+//  Created by John Slomka on 2017-05-12.
+//  Copyright © 2017 John Slomka. All rights reserved.
+//
+
+import UIKit
+
+class Garbage: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
