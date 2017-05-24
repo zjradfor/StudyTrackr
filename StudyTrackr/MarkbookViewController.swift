e95d774f894f9f3762665face90d8e5576fa2cdc
Alex Berry <berryalex6@gmail.com>
2017-05-24 09:47:26 -0400

    initial mark book commit

---
+++ StudyTrackr/MarkbookViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MarkbookViewController.swift
+//  StudyTrackr
+//
+//  Created by Alex Berry on 2017-05-24.
+//  Copyright © 2017 John Slomka. All rights reserved.
+//
+
+import UIKit
+
+class MarkbookViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
