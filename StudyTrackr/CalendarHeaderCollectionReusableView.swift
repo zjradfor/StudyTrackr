96830cf2f3f3bca5cfe29e37ed4eaff66702d5ba
jdslomka <jdslomka@zpr-l0007072.local>
2017-05-31 10:28:22 -0400

    Working on buttons

--- StudyTrackr/CalendarHeaderCollectionReusableView.swift
+++ StudyTrackr/CalendarHeaderCollectionReusableView.swift
@@ -10,16 +10,34 @@ import UIKit
 
 //This is the Label being connected to the header class
 class CalendarHeaderCollectionReusableView: UICollectionReusableView {
+    
     @IBOutlet weak var dateHeader: UILabel!
-    @IBAction func rightButton(_ sender: Any) {
+    
+    @IBAction func leftButton(_ sender: UIButton) {
+        buttonIsPressedL = true
+        //month -= month
+    }
+    @IBOutlet weak var leftButton: UIButton!
+    @IBAction func rightButton(_ sender: UIButton) {
+        buttonIsPressedR = true
+        print("buttonActivated")
+        //month += month
+    }
+    @IBOutlet weak var rightButton: UIButton!
+    
+    
+    
+    
+    /*@IBAction func rightButton(_ sender: Any) {
      buttonIsPressedR = true
-        month += month
+        print("buttonActivated")
+        //month += month
     }
     @IBAction func leftButton(_ sender: Any) {
      buttonIsPressedL = true
-        month -= month
+        //month -= month
     }
-    
+    */
     
     
     }
